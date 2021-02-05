8d8cf2734ce93ccec13d84eb2597669129f6214c
SorairoGozen <celestecaterino@gmail.com>
2021-02-05 22:11:58 +0100

    updated style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {Container} from 'react-bootstrap';
 const App= () => {
   return (
     <>
-    <h1>Welcome to proshop</h1>
+   
+      <Header/>
+      <main>
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+      <Footer/>
+   
+    
     </>
   );
 }
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Navbar, Nav, Container} from 'react-bootstrap';
+const Header = () => {
+    return (
+        <header>
+            <div className="d-none d-lg-block d-xl-block">
+                <Navbar className="bd-navbar text-light navbar-dark" expand="lg">
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Brand href="#home">E-commerce</Navbar.Brand>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto mr-5">
+                            <Nav.Link href="#home"><i class="fas fa-shopping-cart"></i></Nav.Link>
+                            <Nav.Link href="#link">Sign in <i className="fas fa-sign-in-alt ml-1"></i></Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+            <div className="d-lg-none d-xl-none">
+                header mobile
+            </div>
+        </header>
+    )
+}
+
+export default Header
+//rafce
