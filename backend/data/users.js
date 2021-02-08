64004a6ea135948351490bd641f0bbe708d7361c
SorairoGozen <celestecaterino@gmail.com>
2021-02-08 23:20:15 +0100

    added seeder

---
+++ backend/data/users.js
@@ -0,0 +1,23 @@
+const users=[
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: 'password',
+        isAdmin: true
+    },
+    {
+        name: 'john Doe',
+        email: 'john@example.com',
+        password: 'password',
+
+    },
+    {
+        name: 'jane doe',
+        email: 'jane@example.com',
+        password: 'password',
+
+    },
+   
+]
+
+export default users
---
+++ backend/seeder.js
@@ -0,0 +1,40 @@
+import dotenv from 'dotenv'
+import products from './data/products.js'
+import users from './data/users.js'
+import Product from './models/Product.js'
+import User from './models/User.js'
+import Order from './models/Order.js'
+import connectDb from './db.js'
+dotenv.config()
+connectDb()
+const destroyData= async()=>{
+    try{
+        await User.deleteMany()
+        await Product.deleteMany()
+        await Order.deleteMany()
+        
+        
+        process.exit()
+    }catch(error){
+
+    }
+}
+const importData=async ()=>{
+    const createdUsers=await User.insertMany(users)
+    const adminUser=createdUsers[0]._id
+    const addProducts= products.map((product)=>{
+        return {
+            ...product,
+            user: adminUser
+        }
+    })
+    await Product.insertMany(addProducts)
+}
+
+destroyData()
+if(process.argv[2] == '-d'){
+    destroyData()
+}else{
+    importData()
+}
+
