1b5b6358660bef17a338eeb8fbe001f325fcf06c
SorairoGozen <celestecaterino@gmail.com>
2021-02-11 00:34:52 +0100

    Error handler has been created

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDb from './db.js'
 import productRoutes from './routes/products.js'
+import {notFound, errorhandler} from './middleware/errorHandler.js'
 
 dotenv.config()
 connectDb()
@@ -13,4 +14,6 @@ app.get('/',(req,res)=>{
     res.json('Api running')
 });
 app.use('/api/products',productRoutes)
+app.use(notFound)
+app.use(errorhandler)
 app.listen(PORT, console.log('Server running...'))
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,13 +1,19 @@
 import express from 'express'
 import Product from '../models/Product.js'
+import asyncHandler from 'express-async-handler'
 const router=express.Router()
 
 router.get('/',async (req,res)=>{
     const products=await Product.find({});
     res.json(products)
 });
-router.get('/:id',async (req,res)=>{
-    const product=await Product.findById(req.params.id)
-    res.json(product)
-});
+router.get('/:id',asyncHandler(async (req,res)=>{
+    
+        const product=await Product.findById(req.params.id)
+        if(!product){
+            res.status(404)
+            throw new Error('product not found')
+        }
+        res.json(product)
+}));
 export default router
